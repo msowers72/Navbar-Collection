f05f8f17391acd624abb7c8d1736888d2e6d90ff
Mike Sowers <msowers72@yahoo.com>
2025-06-18 14:44:23 -0400

    stlyed the mobile menu

---
+++ Glassmorphism/index.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('fa-xmark')
+    navbar.classList.toggle('active');
+})
